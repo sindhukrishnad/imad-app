4ad1b8ef6af81d96119518078461be2734180af5
Dhanush krishna R <36594676+sindhukrishnad@users.noreply.github.com>
2018-03-04 20:46:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function (req,res){
-    res.send("If article 1 is requested then it will served here.");
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article 2',function (req,res){
     res.send("If article 2 is requested then it will served here.");
